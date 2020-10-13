cc88dd4063ff9d3462f00d21c7fdc9eafca4d139
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-10-13 17:49:43 +0300

    Init commit

---
+++ multi-project/api/src/main/java/api/App.java
@@ -0,0 +1,9 @@
+package api;
+
+import core.Utils;
+
+class App {
+    public static void main(String[] args) {
+        System.out.println("Is 12 and 79 positive: " + Utils.isAllPositiveNumbers("12", "79"));
+    }
+}
---
+++ multi-project/core/src/main/java/core/Utils.java
@@ -0,0 +1,11 @@
+package core;
+
+import utils.StringUtils;
+
+import java.util.Arrays;
+
+public class Utils {
+    public static boolean isAllPositiveNumbers(String ... str) {
+        return Arrays.stream(str).allMatch(StringUtils::isPositiveNumber);
+    }
+}
